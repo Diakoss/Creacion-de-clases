ef33c2575c443ed64bb12e85ce5949d5385ad793
Diakoss <xmaximusz@outlook.com>
2024-02-13 16:58:14 -0600

    Creación de clases Usuario y Video Profesor Alfonso

--- src/main/java/com/mycompany/youtube/Youtube.java
+++ src/main/java/com/mycompany/youtube/Youtube.java
@@ -9,5 +9,8 @@ public class Youtube {
         usuario.subirVideo(video);
         usuario.mostrarInfoUsuario();
         
+        Usuario usuario2=new Usuario ("Blanco");
+        Comentario comment1=new Comentario("Feo tu modo", usuario2);
+        System.out.println("El usuario" + usuario2.getNombre()+ "Publico el comentario"+comment1.getContenido());
     }
 }
