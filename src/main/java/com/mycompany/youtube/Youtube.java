81d589aec8a3b3cd68367063f4a02eeba61ae262
Diakoss <xmaximusz@outlook.com>
2024-02-13 17:02:03 -0600

    Creación de clases Usuario y Video Profesor Alfonso

--- src/main/java/com/mycompany/youtube/Youtube.java
+++ src/main/java/com/mycompany/youtube/Youtube.java
@@ -4,13 +4,13 @@ package com.mycompany.youtube;
 public class Youtube {
 
     public static void main(String[] args) {
-        Usuario usuario = new Usuario ("manzana");
-        Video video = new Video ("aprendiendo java",2400, "http;//manzana.com");
+        Usuario usuario = new Usuario ("Negro");
+        Video video = new Video ("Aprendiendo Java",2400, "http;//negro.com");
         usuario.subirVideo(video);
         usuario.mostrarInfoUsuario();
         
         Usuario usuario2=new Usuario ("Blanco");
-        Comentario comment1=new Comentario("Feo tu modo", usuario2);
+        Comentario comment1=new Comentario("Puto el que lo lea", usuario2);
         System.out.println("El usuario" + usuario2.getNombre()+ "Publico el comentario"+comment1.getContenido());
     }
 }
